1230c35c3eff2416862d79c00d7c38db0e8cf52d
TimbirICT <tmiddlebrooks99@gmail.com>
2023-10-17 17:51:20 -0500

    added several changes and forgot to commit them, changed colors and completed functionallity

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
@@ -43,20 +42,26 @@
 
 $(function () {
   function createTimeBlock(hour) {
-    var timeBlock = $("<div>").addClass("row time-block").attr("id", "hour-" + hour);
-    var hourColumn = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(hour);
-    var textarea = $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3");
-    var saveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+    var timeBlock = $("<div>")
+      .addClass("row time-block")
+      .attr("id", "hour-" + hour);
+    var hourColumn = $("<div>")
+      .addClass("col-2 col-md-1 hour text-center py-3")
+      .text(hour);
+    var textarea = $("<textarea>")
+      .addClass("col-8 col-md-10 description")
+      .attr("rows", "3");
+    var saveButton = $("<button>")
+      .addClass("btn saveBtn col-2 col-md-1")
+      .attr("aria-label", "save");
     var saveIcon = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true");
     saveButton.append(saveIcon);
     timeBlock.append(hourColumn, textarea, saveButton);
     return timeBlock;
   }
 
-  
   $(".container-lg").empty();
 
-  
   for (var hour = 9; hour <= 11; hour++) {
     var amPm = "AM";
     var timeLabel = hour + amPm;
@@ -64,8 +69,7 @@ $(function () {
     $(".container-lg").append(timeBlock);
   }
 
-  
-  for (var hour = 12; hour <= 17; hour++) {
+  for (var hour = 12; hour <= 19; hour++) {
     var amPm = "PM";
     var displayHour = hour <= 12 ? hour : hour - 12;
     if (displayHour === 0) {
@@ -74,34 +78,32 @@ $(function () {
     var timeLabel = displayHour + amPm;
     var timeBlock = createTimeBlock(timeLabel);
     $(".container-lg").append(timeBlock);
+    console.log(timeLabel);
   }
-
-
 });
 
 $(function () {
-
   $(".saveBtn").on("click", function () {
     var timeBlockId = $(this).closest(".time-block").attr("id");
     var userInput = $(this).siblings("textarea").val();
     localStorage.setItem(timeBlockId, userInput);
   });
 
-  
   function updateHourStyles() {
     var currentHour = dayjs().hour();
+    currentHour = (currentHour + 12) % 12 || 12;
 
     $(".time-block").each(function () {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
       $(this).removeClass("past present future");
 
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
+      if (blockHour > currentHour) {
+        $(this).addClass("future");
       } else if (blockHour === currentHour) {
         $(this).addClass("present");
       } else {
-        $(this).addClass("future");
+        $(this).addClass("past");
       }
     });
   }
@@ -121,5 +123,5 @@ $(function () {
 
   loadUserInput();
 
-  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY h:mma"));
 });
